b3700ee3ce637b931f229239d5d206fbf4444b1e
Kaushik Bhadra <kbhadra100@gmail.com>
2022-08-28 22:52:54 +0530

    add modal with different dom create-portal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react'
 
 import './App.css'
+import Cart from './components/Cart/Cart'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
+
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <Meals />
     </Fragment>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import Modal from '../UI/Modal'
 import styles from './Cart.module.css'
 const Cart = () => {
   const cartItems = (
     <ul className={styles['cart-items']}>
       {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   )
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
@@ -19,7 +20,7 @@ const Cart = () => {
         <button className={styles['button-alt']}>Close</button>
         <button className={styles.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
