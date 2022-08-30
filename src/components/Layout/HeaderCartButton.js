ed84d3a1378f478e2d1c9c33f463eb8c8c983a64
Kaushik Bhadra <kbhadra100@gmail.com>
2022-08-30 22:49:38 +0530

    add some more style

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import CartIcon from '../Cart/CartIcon'
 import CartContext from '../../store/cart-context'
 import styles from './HeaderCartButton.module.css'
 const HeaderCartButton = (props) => {
+  const [btnIsHighlight, setBtnIsHighlight] = useState(false)
   const cartCtx = useContext(CartContext)
-  const numberOfCartItems = cartCtx.items.reduce((prevNumber, item) => {
+  const { items } = cartCtx
+  const numberOfCartItems = items.reduce((prevNumber, item) => {
     return prevNumber + item.amount
   }, 0)
+  const btnStyles = `${styles.button} ${btnIsHighlight ? styles.bump : ''}`
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlight(true)
+    const timer = setTimeout(() => {
+      setBtnIsHighlight(false)
+    }, 300)
+
+    //clean up function
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
+      <span className={styles.title}>Your Cart</span>
       <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   )
